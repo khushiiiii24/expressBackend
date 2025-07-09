d41c3d9448311868e517a70ef5a77f7b75df2f4e
khushiiiii24 <khushisogani24@gmail.com>
2025-07-09 16:06:31 +0530

    first commit

---
+++ express.server.js
@@ -0,0 +1,39 @@
+import express from "express"
+const app=express();
+const port=4000;
+
+const data=[
+    {id:1,name:"dog"},
+    {id:2,name:"cat"},
+    {id:3,name:"elephant"},
+    {id:4,name:"giraff"},
+]
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("hello to my first API")
+})
+app.post("/",(req,res)=>{
+    res.send(data);
+})
+
+app.post("/name/add",(req,res)=>{
+    const newName=req.body;
+    data.push(newName)
+    res.send(data)
+})
+
+app.put("/name/update/:id",(req,res)=>{
+    let {id}=req.params
+    id=Number(id)
+
+    const newData=data.map((obj)=>{
+        return obj.id===id?{...obj,...req.body}:obj;
+    });
+    res.send(newData);
+})
+
+
+ app.listen(port,()=>{
+    console.log("Server is started "+port);
+    
+ })
